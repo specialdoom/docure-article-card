f157a2f12808e8240f85d6e640e1c20b5e29f9d4
Bogdan Bledea <bledea.bogdan97@gmail.com>
2021-03-27 14:56:02 +0200

    Update README. Update component with slots

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-#docure-article-card
+### docure-article-card
+
+```js
+npm i @specialdoom/docure-article-card
+
+```
+### usage
+
+```html
+<docure-article-card>
+  <span slot="title">Title</span>
+  <span slot="description">Description</span>
+</docure-article-card>
+```
+
+### slots
+- _slot="title"_ to display the title of the article
+- _slot="description"_ to display the description of the article
+
+You can use any of the html elements instead of _span_ element, but you need to add the slot attribute depending on what you want to display.
+
+### example 
+
+![Presentation](presentation.png)
--- src/docure-article-card.svelte
+++ src/docure-article-card.svelte
@@ -1,15 +1,11 @@
 <svelte:options tag="docure-article-card" />
 
-<script>
-  export let title = "";
-  export let description = "";
-</script>
-
-
 <div class="docure-article-card">
-  <p class="docure-article-card-title">{title}</p>
+  <p class="docure-article-card-title">
+    <slot name="title" />
+  </p>
   <div class="docure-article-card-description">
-    {description}
+    <slot name="description" />
   </div>
 </div>
 
@@ -21,15 +17,4 @@
     box-shadow: 0 2px 7px 2px #bfbfbf;
     padding: 14px;
   }
-
-  .docure-article-card-title{
-    color: #4392f1;
-    font-size: 18px;
-  }
-
-  .docure-article-card-title:hover{
-    text-decoration: underline;
-    cursor: pointer;
-  }
-
 </style>
